4ed008d454060fee56d6d894cb0276ae857e5bdc
lagatchell <lagatche@geneva.edu>
2017-04-11 19:18:45 -0400

    Steel Thread App and Html page

---
+++ TestSteelThreadApp/TestSteelThreadApp/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace TestSteelThreadApp
+{
+    [Activity(Label = "TestSteelThreadApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            TextView myText = FindViewById<TextView>(Resource.Id.MyText);
+
+            using (var client = new HttpClient())
+            {
+                var url = string.Format("https://funcsteelthread.azurewebsites.net/api/HttpTriggerCSharp1?code=mjw3z9kRhdAcdo6poYaA5aeXIvOaD8AvfWfTTZSE3P71sHcxvhffng==");
+
+                var content = new StringContent("{name: '" + myText.Text + "'}", Encoding.UTF8, "application/json");
+
+                var result = client.PostAsync(url, content).Result.EnsureSuccessStatusCode().Content.ReadAsStringAsync().Result;
+
+                Toast.MakeText(Application.Context, JsonConvert.DeserializeObject<string>(result), ToastLength.Long).Show();
+
+            }
+        }
+    }
+}
+
